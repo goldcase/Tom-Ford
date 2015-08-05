9045615b363da2e9c8ab015a441ea5ab69d4fbcf
Johnny Chang <johnny@johnnychang.com>
2015-08-04 21:15:59 -0500

    added route skeleton

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 #!flask/bin/python
 # The above line is to specify which program to run this file with. We have virtualenv installed, so use that version of python.
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 # TODO: Mock out some analytics requests.
 
-@app.route('/tomford/api/v1.0/', methods=['GET'])
+@app.route("/tomford/api", methods=["GET"])
 def index():
-    return "Welcome to the Tom Ford rap analytics API!"
+    return jsonify({ "message": "Welcome to the Tom Ford rap analytics API!" })
+
+@app.route("/tomford/api/syllables/", methods=["GET"])
+def syllables():
+	return jsonify({ "count": 3, "syllables": ["tom", "ford"] })
+
+@app.route("/tomford/api/detect/", methods=["GET"])
+def detection():
+	return jsonify({ "message": "Detecting all schemes. Welcome." })
+
+@app.route("/tomford/api/detect/perfect/", methods=["GET", "POST"])
+def detect_perfect():
+	return jsonify({ "message": "Detecting perfect rhymes." })
+
+@app.route("/tomford/api/detect/multi/", methods=["GET", "POST"])
+def detect_multis():
+	return jsonify({ "message": "Detecting multis." })
 
 if __name__ == '__main__':
     app.run(debug=True)
