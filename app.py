0862b53b2f35d7f788fbb39fe887b7b1bd35feb0
Johnny Chang <johnny@johnnychang.com>
2015-08-05 22:21:27 -0500

    added syllable counting method and API link

--- app.py
+++ app.py
@@ -12,10 +12,12 @@ app = Flask(__name__)
 
 transcr = cmudict.dict()
 
+
+
 # TODO: Mock out some analytics requests.
 
 # Normalizes the string, takes out any punctuation.
-# Returns a list of words.
+# Returns: a list of words.
 def normalize_and_split_string(s):
 	# Deletes punctuation using string.translate tables, then lowers the case and splits it into a list.
 	# Creates table mapping punctuation to None
@@ -27,8 +29,41 @@ def normalize_and_split_string(s):
 def transcribe_string(s):
 	return [transcr[word][0] for word in normalize_and_split_string(s)]
 
+# Wraps calling transcribe_string on every line in list l
 def transcribe_list(l):
 	return [transcribe_string(line) for line in l]
+
+# Returns the syllables and count of a line of tokenized lyrics.
+def syllables_line(l):
+	syllables = []
+	syllable_count = 0
+
+	for word in l:
+		# Temp represents one syllable.
+		temp = []
+		phoneme_count = 0
+
+		for phoneme in word:
+			phoneme_count += 1
+			temp.append(phoneme)
+			# If the last character in that phoneme is a digit, it's a syllable.
+			if phoneme[-1].isdigit():
+				# Add the syllable to the return list and clear temp.
+				syllables.append(temp)
+				temp = []
+				# Increment the number of syllables.
+				syllable_count += 1
+
+		# Add last few syllables on.
+		if len(word) == phoneme_count and not(len(temp) == 0):
+			syllables[-1] += temp
+
+
+	return {"syllables": syllables, "count": syllable_count}
+
+# Returns list of objects of lists of syllables of that line without word boundaries and associated count.
+def get_syllables_and_count(tokenized_lyrics):
+	return [syllables_line(line) for line in tokenized_lyrics]
 
 @app.errorhandler(404)
 def not_found(error):
@@ -38,19 +73,32 @@ def not_found(error):
 def index():
     return jsonify({ "message": "Welcome to the Tom Ford rap analytics API!" })
 
+# Params: list of strings in JSON.
+# Returns: a transcribed list of the passed-in lyrics.
 @app.route("/tomford/api/tokenize", methods=["POST"])
 def tokenize():
 	# Abort if there is no request or if the request doesn't contain the lyrics property.
 	if not request.json or not "lyrics" in request.json:
 		abort(400)
+
 	lyrics = request.json["lyrics"]
 
-	return jsonify({ "tokens": transcribe_list(lyrics) })
+	# Call transcribe_list on the lyrics and return the JSON result
+	return jsonify({ "tokenized_lyrics": transcribe_list(lyrics) })
 
-@app.route("/tomford/api/syllables/", methods=["GET"])
+# Params: tokenized_lyrics property inside JSON
+# Returns: syllable object in 
+@app.route("/tomford/api/syllables/", methods=["POST"])
 def syllables():
-	return jsonify({ "count": 3, "syllables": ["tom", "ford"] })
+	if not request.json or not "tokenized_lyrics" in request.json:
+		abort(400)
 
+	tokenized_lyrics = request.json["tokenized_lyrics"]
+
+	return jsonify({ "syllables_by_line": get_syllables_and_count(tokenized_lyrics) })
+
+# Params:
+# Returns:
 @app.route("/tomford/api/detect/", methods=["GET"])
 def detection():
 	return jsonify({ "message": "Detecting all schemes. Welcome." })
